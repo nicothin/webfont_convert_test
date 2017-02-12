fbb6713963bc4039d0aeb23ebdcd434b439fb506
Nikolay Gromov <nicothin@gmail.com>
2017-02-12 11:02:59 +0300

    Новая стартовая площадка

--- script.js
+++
@@ -1,9 +0,0 @@
-var collapseHeaders = document.querySelectorAll('.collapse__header');
-console.log(collapseHeaders.length);
-for (var i = 0; i < collapseHeaders.length; i++) {
-  var collapseHeader = collapseHeaders[i];
-  collapseHeader.onclick = function() {
-    this.parentNode.classList.toggle('collapse--hidden');
-    console.log(this.parentNode);
-  };
-}
